eabdb50e0824538db1e5ab985455d0168ce94f24
King Elisha <ek.chibueze@gmail.com>
2020-07-17 14:17:42 +0100

    feat: implemented add user route

---
+++ src/constants/socket.events.ts
@@ -0,0 +1,3 @@
+export enum SocketEvents {
+  ADD_USER = "ADD_USER",
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,9 +2,17 @@ import { Router } from "express";
 
 export const userRoute = Router();
 
-userRoute.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: [],
+userRoute
+  .get("/", (req, res) => {
+    res.status(200).json({
+      success: true,
+      data: [],
+    });
+  })
+  .post("/", (req, res) => {
+    const { body } = req;
+
+    res.status(201).json({
+      success: typeof body === "object",
+    });
   });
-});
