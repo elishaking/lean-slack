2d8c6f246e81b51585b9d07a57adf48d21b8c0f9
King Elisha <ek.chibueze@gmail.com>
2020-07-17 14:05:08 +0100

    test: add user endpoint test

--- tests/routes/users.test.ts
+++ tests/routes/users.test.ts
@@ -15,3 +15,19 @@ describe("GET Endpoints", () => {
     done();
   });
 });
+
+describe("POST Endpoints", () => {
+  it("should add new user", async (done) => {
+    const newUser = {
+      name: "King E",
+      date: Date.now(),
+    };
+    const { status, body } = await request(app).post("/users").send(newUser);
+    expect(status).toEqual(201);
+    expect(typeof body === "object").toBe(true);
+    expect(body).toHaveProperty("success");
+    expect(body.success).toBe(true);
+
+    done();
+  });
+});
