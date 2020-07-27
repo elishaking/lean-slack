73d656759fe2c672ffc4af07b1e4afa6df7f5311
King Elisha <ek.chibueze@gmail.com>
2020-07-27 12:58:09 +0100

    test(routes): restructured tests

--- tests/routes/users.test.ts
+++ tests/routes/users.test.ts
@@ -1,45 +1,49 @@
 import request from "supertest";
 
 import app from "../../src/app";
+import * as db from "../db";
 import { userService } from "../../src/services";
 
-describe("GET Endpoints", () => {
-  it("should return all users", async (done) => {
-    const { status, body } = await request(app).get("/users");
-    expect(status).toEqual(200);
-    expect(typeof body === "object").toBe(true);
-    expect(body).toHaveProperty("success");
-    expect(body).toHaveProperty("payload");
-    expect(body.success).toBe(true);
-    expect(Array.isArray(body.payload)).toBe(true);
-
-    done();
-  });
-});
-
-const newUser1: any = {
-  name: "King",
-};
-
-describe("POST Endpoints", () => {
+describe("User Route", () => {
   afterAll((done) => {
     userService
       .clear()
+      .then(() => db.disconnectDb())
       .then(() => done())
       .catch((err) => done(err));
   });
 
-  it("should add new user", async (done) => {
-    const { status, body } = await request(app).post("/users").send(newUser1);
+  describe("GET Endpoints", () => {
+    it("should return all users", async (done) => {
+      const { status, body } = await request(app).get("/users");
+      expect(status).toEqual(200);
+      expect(typeof body === "object").toBe(true);
+      expect(body).toHaveProperty("success");
+      expect(body).toHaveProperty("payload");
+      expect(body.success).toBe(true);
+      expect(Array.isArray(body.payload)).toBe(true);
 
-    expect(status).toEqual(201);
-    expect(typeof body === "object").toBe(true);
-    expect(body).toHaveProperty("success");
-    expect(body.success).toBe(true);
-    expect(body).toHaveProperty("payload");
-    expect(typeof body.payload === "object").toBe(true);
-    expect(body.payload.name).toEqual(newUser1.name);
+      done();
+    });
+  });
 
-    done();
+  const newUser1: any = {
+    name: "King",
+  };
+
+  describe("POST Endpoints", () => {
+    it("should add new user", async (done) => {
+      const { status, body } = await request(app).post("/users").send(newUser1);
+
+      expect(status).toEqual(201);
+      expect(typeof body === "object").toBe(true);
+      expect(body).toHaveProperty("success");
+      expect(body.success).toBe(true);
+      expect(body).toHaveProperty("payload");
+      expect(typeof body.payload === "object").toBe(true);
+      expect(body.payload.name).toEqual(newUser1.name);
+
+      done();
+    });
   });
 });
